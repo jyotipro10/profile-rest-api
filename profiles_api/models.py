24284bd7df575347f69e0f5914936c24038e7ada
Jyoti Prakash <jyotipro333@gmail.com>
2021-05-18 22:02:59 +0530

    Added custom profile manager and migrated(small changes)

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -13,7 +13,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError("User don't have an email address")
 
         email = self.normalize_email(email)
-        user = self.model(email = email, name = name)
+        user = self.model(email = email, name = name,)
 
         user.set_password(password)
         user.save(using = self._db)
@@ -41,7 +41,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Get fullname of user"""
